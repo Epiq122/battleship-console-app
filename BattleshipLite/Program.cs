189a051591c3429dcb695aaad9e2ce0026de72c3
Rob Gleason <robgleason122@gmail.com>
2024-08-14 21:31:02 -0700

    created game logic class, and added initialize grid and add grid

---
+++ BattleshipLite/Program.cs
@@ -0,0 +1,51 @@
+﻿using BattleshipLiteLibrary;
+using BattleshipLiteLibrary.Models;
+using System;
+
+namespace BattleshipLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WelcomeMessage();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("created by Rob Gleason");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer()
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            // Ask the user for their name
+            output.Username = AskForUserName();
+            // Load up the shot grid
+            GameLogic.InitializeGrid(output);
+            // ask the user for their 5 ship placements
+            // Clear
+        }
+
+        private static string AskForUserName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+
+            } while (model.ShipLocations.Count < 5);
+        }
+    }
+
+}
---
+++ BattleshipLiteLibrary/GameLogic.cs
@@ -0,0 +1,53 @@
+﻿using BattleshipLiteLibrary.Models;
+using System.Collections.Generic;
+
+namespace BattleshipLiteLibrary
+{
+    public static class GameLogic
+    {
+        public static void InitializeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            foreach (var letter in letters)
+            {
+                foreach (var number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+
+
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+
+            model.ShotGrid.Add(spot);
+
+
+        }
+    }
+}
